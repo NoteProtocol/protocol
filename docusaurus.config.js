19936fb944d8ccfe6bd491708c8e2d4a2a7e67e1
Long Li <lilong@gmail.com>
2024-01-02 16:11:08 +0900

    add missed contents

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/favicon.png',
 
   // Set the production url of your site here
-  url: 'https://noteprotoco.org',
+  url: 'https://noteprotocol.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
